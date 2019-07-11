a64ecc8a874e731b88fb9a46028c6493e837427e
Luca Denti <ldenti92@gmail.com>
2019-07-11 21:55:14 +0200

    Polishing

--- parse_vcf.cpp
+++ parse_vcf.cpp
@@ -7,25 +7,35 @@
 
 #include "vcf.h"
 #include "kstring.h"
-#include "variant.hpp"
+// #include "variant.hpp"
 
 using namespace std;
 
-struct GenoType {
-  int a1, a2;
+struct GT { // This name is already used in variant.hpp
+  uint8_t a1, a2;
   bool phased;
 
-  GenoType() {
+  GT() {
     a1 = 0;
     a2 = 0;
     phased = true;
   }
 
-  GenoType(int a1_, int a2_, bool phased_) {
+  GT(uint8_t a1_, uint8_t a2_, bool phased_) {
     a1 = a1_;
     a2 = a2_;
     phased = phased_ || a1 == a2;
-    cout << a1 << "-" << a2 << "-" << phased << endl;
+  }
+
+  string to_str() {
+    return phased ? to_string(a1)+"|"+to_string(a2) : to_string(a1)+"/"+to_string(a2);
+  }
+
+  void print() {
+    if(phased)
+      cout << (int)a1 << "|" << (int)a2 << endl;
+    else
+      cout << (int)a1 << "/" << (int)a2 << endl;
   }
 };
 
@@ -55,10 +65,9 @@ char* get_samples(char *vcf_line) {
   return vcf_line + offset + fmt_size + 1;
 }
 
-GenoType extract_genotype(char* gt) {
-  cout << gt << endl;
+GT extract_genotype(char* gt) {
   if(!strcmp(gt, ".")) {
-    return GenoType();
+    return GT();
   }
   bool phased = strchr(gt, '/') != NULL;
   int all1, all2;
@@ -66,68 +75,74 @@ GenoType extract_genotype(char* gt) {
   all1 = atoi(all);
   all = strtok(NULL,"/|");
   all2 = all==NULL ? all1 : atoi(all);
-  cout << "@ " << all1 << "-" << all2 << "-" << phased << endl;
-  return GenoType(all1, all2, phased);
+  return GT(all1, all2, phased);
 }
 
-void parse_format_field(char *vcf_line) {
+void parse_format_field(char *vcf_line, const int &n_samples) {
   char *samples = get_samples(vcf_line);
-  
+
+  vector<GT> genotypes (n_samples);
+  int i = 0;
   for(;;) {
     char *next_tab = strchr(samples, '\t');
-    char *gt = strtok(samples,":\t");
-    GenoType g = extract_genotype(gt);
+    char *gtc = strtok(samples,":\t");
+    GT gt = extract_genotype(gtc);
+    genotypes[i] = gt;
     if(next_tab == NULL) break;
     samples = next_tab+1;
   }
-  // while (sample != NULL) {
-  //   cout << strlen(sample) << " " << sample << endl;
-    
-  //   // char *gt = strtok(sample,":");
-  //   // cout << gt << endl;
-
-  //   sample = strtok(NULL,"\t");
-  // }
 }
 
-void analyze(vector<kstring_t*> &vcf_lines, vector<bcf1_t> &vcf_records, bcf_hdr_t *vcf_header, const int &n_threads) {
-#pragma omp parallel for num_threads (n_threads) shared (vcf_header, vcf_lines, vcf_records)
+void analyze(vector<kstring_t*> &vcf_lines, const int &n_samples, const int &n_threads) {
+#pragma omp parallel for num_threads (n_threads) shared (vcf_lines, n_samples)
   for(int i=0; i<(int)vcf_lines.size(); ++i) {
-    parse_format_field(vcf_lines[i]->s);
+    parse_format_field(vcf_lines[i]->s, n_samples);
     // Variant v(vcf_header, &vcf_records[i], "AF");
     // cerr << v.idx << " " << v.ref_pos << endl;
   }
-  for(auto &s : vcf_lines) {
-    ks_free(s);
-  }  
 }
 
-int parse_vcf_htslib(char* vcf_path, const int &n_threads) {
+int parse_vcf_htslib_mixed(char* vcf_path, const int &n_threads) {
   htsFile *vcf = bcf_open(vcf_path, "r");
   bcf_hdr_t *vcf_header = bcf_hdr_read(vcf);
+  int n_samples = bcf_hdr_nsamples(vcf_header);
   bcf1_t *vcf_record = bcf_init();
   vcf_record->max_unpack = BCF_UN_INFO;
 
   vector<kstring_t*> vcf_lines;
-  vector<bcf1_t> vcf_records;
   while (bcf_read(vcf, vcf_header, vcf_record) == 0) {
     bcf_unpack(vcf_record, BCF_UN_INFO); // here we unpack till INFO field
-    vcf_records.push_back(*vcf_record);
+
     kstring_t *ks = new kstring_t();
     ks_initialize(ks);
     kputsn(vcf->line.s, vcf->line.l, ks);
     vcf_lines.push_back(ks);
 
     if(vcf_lines.size() == 20000) {
-      analyze(vcf_lines, vcf_records, vcf_header, n_threads);
+      analyze(vcf_lines, n_samples, n_threads);
       vcf_lines.clear();
-      vcf_records.clear();
     }
   }
   if(!vcf_lines.empty()) {
-    analyze(vcf_lines, vcf_records, vcf_header, n_threads);
+    analyze(vcf_lines, n_samples, n_threads);
     vcf_lines.empty();
-    vcf_records.empty();
+  }
+
+  bcf_hdr_destroy(vcf_header);
+  bcf_destroy(vcf_record);
+  bcf_close(vcf);
+
+  return 0;
+}
+
+int parse_vcf_htslib_ALL(char* vcf_path, const int &n_threads) {
+  htsFile *vcf = bcf_open(vcf_path, "r");
+  bcf_hdr_t *vcf_header = bcf_hdr_read(vcf);
+  bcf1_t *vcf_record = bcf_init();
+  vcf_record->max_unpack = BCF_UN_ALL;
+
+  while (bcf_read(vcf, vcf_header, vcf_record) == 0) {
+    bcf_unpack(vcf_record, BCF_UN_ALL);
   }
 
   bcf_hdr_destroy(vcf_header);
@@ -140,7 +155,7 @@ int parse_vcf_htslib(char* vcf_path, const int &n_threads) {
 int main(int argc, char *argv[]) {
   char* vcf_path = argv[1];
   int n_threads = atoi(argv[2]);
-  parse_vcf_htslib(vcf_path, n_threads);
+  parse_vcf_htslib_mixed(vcf_path, n_threads);
   cerr << "END" << endl;
   return 0;
 }
