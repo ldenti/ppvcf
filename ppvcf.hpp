a5607c65ec54885d38ea5d536fb50972ffe25fd8
Luca Denti <ldenti92@gmail.com>
2020-02-25 15:33:56 +0000

    Drops dependency on htslib

--- ppvcf.hpp
+++ ppvcf.hpp
@@ -1,16 +1,18 @@
 #ifndef _PPVCF_HPP_
 #define _PPVCF_HPP_
 
+#include <assert.h>
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <numeric>
+#include <sstream>
+#include <string.h>
 #include <string>
 #include <tuple>
 #include <vector>
 
 #include <omp.h>
-
-#include "htslib/vcf.h"
 
 /*---------- Auxiliary structs ----------*/
 //!  Auxiliary struct
@@ -85,85 +87,21 @@ private:
   std::vector<GT> genotypes;
 
   /**
-   * Extract the filter field from record and store it as a string.
+   * Convert the info field in a map<string, string>.
    *
-   * @param header is the htslib header
-   * @param record is a htslib record
+   * @param info is the info field
    **/
-  void store_filter(bcf_hdr_t *header, bcf1_t *record) {
-    // TODO: store filter differently (not as a single string) if we want to
-    // allow filtering
-    filter = "";
-    if (record->d.n_flt) {
-      for (int i = 0; i < record->d.n_flt; ++i) {
-        if (i)
-          filter += ";";
-        filter += header->id[BCF_DT_ID][record->d.flt[i]].key;
-      }
-    } else
-      filter = ".";
-  }
-
-  /**
-   * Extract the additional information from record and store them as a
-   *map<string, string>.
-   *
-   * @param header is the htslib header
-   * @param record is a htslib record
-   **/
-  void store_info(bcf_hdr_t *header, bcf1_t *record) {
-    for (int i = 0; i < record->n_info; ++i) {
-      bcf_info_t *info_field = &record->d.info[i];
-      int key_idx = info_field->key;
-      int type = info_field->type;
-      const char *key = header->id[BCF_DT_ID][key_idx].key;
-
-      int ndst = 0;
-      std::string value = "";
-      if (type == BCF_BT_NULL) {
-        bool v = bcf_get_info_flag(header, record, key, &v, &ndst);
-        value = std::to_string(v);
-        // FIXME: remember to manage booleans (eg when producing output)
-      } else if (type == BCF_BT_INT8 || type == BCF_BT_INT16 ||
-                 type == BCF_BT_INT32) {
-        int *v = NULL;
-        bcf_get_info_int32(header, record, key, &v, &ndst);
-        value = std::to_string(*v);
-        free(v);
-      } else if (type == BCF_BT_FLOAT) {
-        float *v = NULL;
-        bcf_get_info_float(header, record, key, &v, &ndst);
-        value = std::to_string(*v);
-        free(v);
-      } else if (type == BCF_BT_CHAR) {
-        char *v = NULL;
-        bcf_get_info_string(header, record, key, &v, &ndst);
-        value = std::string(v);
-        free(v);
-      } else {
-        cerr << "Unknown type " << type << " (field " << key << ")" << endl;
-        exit(1);
-      }
-
+  void store_info(const std::string &info_string) {
+    std::vector<std::string> info_tokens;
+    std::istringstream info_stream(info_string);
+    std::string token;
+    while (std::getline(info_stream, token, ';'))
+      info_tokens.push_back(token);
+    for (const auto token : info_tokens) {
+      int eq_pos = token.find("=");
+      std::string key = token.substr(0, eq_pos);
+      std::string value = token.substr(eq_pos + 1);
       info[key] = value;
-
-      /* // Maybe we can adapt this
-         #define BRANCH(type_t, bcf_ht_t) {				\
-         type_t *value = NULL;						\
-         bcf_get_info_values(header, record, key, (void**)(&value), &ndst,
-         bcf_ht_t); \
-         cout << *value << endl;					\
-         }
-         switch(type) {
-         case BCF_BT_INT8: BRANCH(int, BCF_HT_INT); break;
-         case BCF_BT_INT16: BRANCH(int, BCF_HT_INT); break;
-         case BCF_BT_INT32: BRANCH(int, BCF_HT_INT); break;
-         case BCF_BT_FLOAT: BRANCH(float, BCF_HT_REAL); break;
-         case BCF_BT_CHAR: BRANCH(std::string, BCF_HT_STR); break;
-         default: cerr << "Unknown type " << type << endl; exit(1);
-         }
-         #undef BRANCH
-      */
     }
   }
 
@@ -181,27 +119,26 @@ public:
   ~Variant() {}
 
   /**
-   * Extract and store the first 8 fields of record.
+   * Extract and store the first eight fields of a record.
    *
-   * @param header is the htslib header
-   * @param record is a htslib record
+   * @param fields are the 8 mandatory fields of a VCF record
    **/
-  void update_till_info(bcf_hdr_t *header, bcf1_t *record) {
-    seq_name = bcf_hdr_id2name(header, record->rid);
-    pos = record->pos;
-    idx = record->d.id;
-    ref = record->d.allele[0];
+  void update_till_info(const std::vector<std::string> &fields) {
+    seq_name = fields[0];
+    pos = stoi(fields[1]) - 1;
+    idx = fields[2];
+    ref = fields[3];
 
-    for (int i = 1; i < record->n_allele; ++i) {
-      char *curr_alt = record->d.allele[i];
-      if (curr_alt[0] != '<')
-        alts.push_back(std::string(curr_alt));
+    std::istringstream alts_stream(fields[4]);
+    std::string alt;
+    while (std::getline(alts_stream, alt, ',')) {
+      alts.push_back(alt);
     }
 
-    quality = std::to_string(record->qual);
-    quality = quality == "nan" ? "." : quality;
-    store_filter(header, record);
-    store_info(header, record);
+    quality = fields[5];
+    filter = fields[6];
+
+    store_info(fields[7]);
   }
 
   /**
@@ -261,6 +198,31 @@ public:
    * @return the value associated to key k
    **/
   std::string get_info(const std::string &k) const { return info.at(k); }
+
+  /* Just for testing :) */
+  // void print() const {
+  //   std::cout << seq_name << "\t"
+  // 	      << pos+1 << "\t"
+  //     	      << idx << "\t"
+  //     	      << ref << "\t";
+  //   for(uint i = 0; i<alts.size(); ++i) {
+  //     std::cout << alts[i];
+  //     if(i != alts.size()-1)
+  // 	std::cout << ",";
+  //     else
+  // 	std::cout << "\t";
+  //   }
+  //   std::cout << quality << "\t"
+  // 	      << filter << "\t";
+  //   for (std::map<std::string,std::string>::const_iterator it=info.begin();
+  //   it!=info.end(); ++it)
+  //     std::cout << it->first << "=" << it->second << ";";
+  //   std::cout << "\t" << "GT" << "\t";
+
+  //   for(const auto gt : genotypes)
+  //     std::cout << (int)gt.a1 << "|" << (int)gt.a2 << "\t";
+  //   std::cout << "\n";
+  // }
 };
 
 /*---------- VCF class ----------*/
@@ -270,24 +232,23 @@ public:
 */
 class VCF {
 private:
-  /// htslib file
-  htsFile *vcf;
-  /// htslib header
-  bcf_hdr_t *vcf_header;
-  /// htslib record
-  bcf1_t *vcf_record;
+  /// vcf file
+  FILE *vcf;
+  /// vcf header
+  std::string vcf_header;
+  /// single vcf record (temp)
+  str_w_l line;
+  /// vcf records
+  std::vector<str_w_l> vcf_records;
+  /// Parsed variants
+  std::vector<Variant> variants;
 
   /// Total number of samples per variant
   int n_samples;
-  /// Variants read
-  std::vector<Variant> variants;
-  /// Genotype fields as cstring (from "GT" to the end of each line)
-  std::vector<str_w_l> fmt_lines;
   /// Number of variants to read at each iteration
   size_t block_size;
   /// Number of variants read in the last iteration
   size_t to_parse;
-
   /// Number of threads to use
   int n_threads;
 
@@ -299,38 +260,37 @@ public:
    * @param n_threads_ is the number of threads to use
    * @param block_size_ is the number of variants to read at each iteration
    **/
-  VCF(char *vcf_path, const int n_threads_, const int block_size_) {
-    vcf = bcf_open(vcf_path, "r");
-    vcf_header = bcf_hdr_read(vcf);
-    vcf_record = bcf_init();
-    vcf_record->max_unpack = BCF_UN_INFO;
+  VCF(char *vcf_path, const int n_threads_, const int block_size_)
+      : block_size(block_size_), to_parse(0), n_threads(n_threads_) {
 
-    n_samples = bcf_hdr_nsamples(vcf_header);
+    vcf = fopen(vcf_path, "r");
+    if (vcf == NULL) {
+      std::cout << "Error opening VCF file" << std::endl;
+      exit(1);
+    }
 
-    n_threads = n_threads_;
+    // Allocate space for single temp line
+    line.size = 1024 * 20;
+    line.seq = (char *)calloc(line.size, sizeof(char));
 
-    block_size = block_size_;
-    to_parse = 0;
-
-    // Allocate space for fmt_lines
+    // Allocate space for records
     for (size_t i = 0; i < block_size; ++i) {
       str_w_l s;
       s.size = 1024 * 10;
       s.seq = (char *)calloc(s.size, sizeof(char));
-
-      fmt_lines.push_back(s);
+      vcf_records.push_back(s);
     }
+
+    parse_header();
   }
   /**
    * Destructor
    **/
   ~VCF() {
     for (size_t i = 0; i < block_size; ++i) {
-      free(fmt_lines[i].seq);
+      free(vcf_records[i].seq);
     }
-    bcf_hdr_destroy(vcf_header);
-    bcf_destroy(vcf_record);
-    bcf_close(vcf);
+    fclose(vcf);
   }
 
   /**
@@ -343,28 +303,25 @@ public:
     variants.clear();
     uint32_t i = 0;
     uint32_t n = block_size;
-    while (i < block_size && bcf_read(vcf, vcf_header, vcf_record) == 0) {
-      // 1. we unpack till INFO field
-      bcf_unpack(vcf_record, BCF_UN_INFO);
+    while (i < block_size && fgets(line.seq, line.size, vcf) != NULL) {
       variants.push_back(Variant(n_samples));
-      variants.back().update_till_info(vcf_header, vcf_record);
 
-      // 2. we store the format line
-      char *samples = get_samples(vcf->line.s, vcf->line.l + 1);
-      size_t samples_size = vcf->line.l - (samples - vcf->line.s);
-      while (samples_size > fmt_lines[i].size) {
-	cerr << "Reallocating memory for variant " << variants.back().get_idx() << endl;
-        fmt_lines[i].size *= 1.6;
-        fmt_lines[i].seq = (char *)realloc(fmt_lines[i].seq, fmt_lines[i].size);
+      size_t line_len = strlen(line.seq);
+      while (line_len > vcf_records[i].size) {
+        std::cerr << "Reallocating memory for variant "
+                  << variants.back().get_idx() << std::endl;
+        vcf_records[i].size *= 1.6;
+        vcf_records[i].seq =
+            (char *)realloc(vcf_records[i].seq, vcf_records[i].size);
       }
-      strncpy(fmt_lines[i].seq, samples, samples_size);
-      fmt_lines[i].seq[samples_size] = '\0';
+      strncpy(vcf_records[i].seq, line.seq, line_len);
+      vcf_records[i].seq[line_len - 1] = '\0'; // -1 to remove trailing \n
+
       ++i;
     }
     to_parse = i;
 
-    // 3. we parse and store the genotypes in parallel
-    fill_genotypes();
+    parse_block();
 
     return i == n;
   }
@@ -376,45 +333,39 @@ public:
 
 private:
   /**
-   * Extract the genotype information from a vcf line
-   *
-   * @param fmt_line is a line of the input vcf file
-   * @param len is the length of fmt_line
-   * @return a cstring representing the genotypes information
+   * Read and store information from VCF header
    **/
-  char *get_samples(char *const fmt_line, const size_t len) {
-    /*
-     * We need len since fmt_line contains \0 to separate the first 8
-     * fields (htslib replaces \t with \0)
-     */
-    char *gt_start = fmt_line;
-    char *const gt_end = fmt_line + len;
-
-    /*
-     * From VCF format specification: "The first sub-field must always
-     * be the genotype (GT) if it is present. There are no required
-     * sub-fields."
-     */
-    // FIXME: manage :
-    for (; gt_start != gt_end && !(*gt_start == '\0' && *(gt_start+1) == 'G' && *(gt_start+2) == 'T' && *(gt_start+3) == '\t');
-	 ++gt_start)
-      ;
-    ++gt_start; // strip the first \0 left by the for
-
-    // FIXME: manage no GT (ie gt_start == gt_end)
-    return gt_start;
+  void parse_header() {
+    while (fgets(line.seq, line.size, vcf)) {
+      vcf_header += line.seq;
+      if (line.seq[0] == '#' and line.seq[1] == 'C')
+        break;
+    }
+    if (!(line.seq[0] == '#' and line.seq[1] == 'C')) {
+      std::cerr
+          << "Malformed VCF file. I cannot find the header line (#CHROM...)"
+          << std::endl;
+      exit(1);
+    }
+    int i = 0;
+    int n_cols = 0;
+    while (line.seq[i] != '\n') {
+      n_cols += line.seq[i] == '\t';
+      ++i;
+    }
+    n_samples = n_cols - 8;
   }
 
   /**
    * Extracts information from a cstring representing a genotype
    *
    * @param start is a cstring
-   * @return a tuple containing the first allele, the second allele, and the
-   *phased flag
+   * @return a tuple containing the first allele, the second allele,
+   * and the phased flag
    **/
-  tuple<uint8_t, uint8_t, bool> extract_genotype(char *start) {
+  std::tuple<uint8_t, uint8_t, bool> extract_genotype(char *start) {
     if (*start == '.')
-      return tuple<uint8_t, uint8_t, bool>(-1, -1, false);
+      return std::tuple<uint8_t, uint8_t, bool>(-1, -1, false);
 
     uint8_t all1, all2;
     char *start2 = start;
@@ -429,7 +380,7 @@ private:
 
     *start2 = '|'; // don't care, just fix
 
-    return std::move(tuple<uint8_t, uint8_t, bool>(all1, all2, phased));
+    return std::move(std::tuple<uint8_t, uint8_t, bool>(all1, all2, phased));
   }
 
   /**
@@ -440,36 +391,63 @@ private:
    **/
   void fill_variant(const uint32_t var_idx) {
     assert(var_idx < to_parse);
-    char *samples = fmt_lines[var_idx].seq;
 
-    // Drop "GT" at the beginning of the line
-    for (; *samples != '\t'; ++samples)
-      ;
-    ++samples;
-    char *start = samples;
+    char *record = vcf_records[var_idx].seq;
+
+    int field = 0;
+    char *start = record;
     char *end = start;
     bool last = *end == '\0';
-    tuple<uint8_t, uint8_t, bool> gt;
+
+    std::vector<std::string> fixed_fields(8);
+    while (field <= 7) {
+      for (; *end != '\t' && *end != '\0'; ++end)
+        ;
+
+      last = *end == '\0';
+      *end = '\0';
+      fixed_fields[field] = start;
+      ++field;
+
+      *end = last ? '\0' : '\t';
+      start = end + 1;
+      end = start;
+    }
+
+    variants[var_idx].update_till_info(fixed_fields);
+
+    if (last) // No GTs
+      return;
+
+    // Drop "GT" at the beginning of the line
+    // TODO: manage ":"
+    for (; *start != '\t'; ++start)
+      ;
+    ++start;
+    end = start;
+    last = *end == '\0';
+
+    std::tuple<uint8_t, uint8_t, bool> gt;
     while (!last) {
       for (; *end != '\t' && *end != '\0'; ++end)
         ;
       last = *end == '\0';
       *end = '\0';
       gt = extract_genotype(start);
-      variants[var_idx].add_genotype(get<0>(gt), get<1>(gt), get<2>(gt));
+      variants[var_idx].add_genotype(std::get<0>(gt), std::get<1>(gt),
+                                     std::get<2>(gt));
       *end = last ? '\0' : '\t';
       start = end + 1;
       end = start;
     }
-    // TODO: manage ":"
   }
 
   /**
-   * Extracts and store the genotypes of all the variants read in the
-   * last iteration
+   * Extracts and store the information associated to each variant of
+   * the current block
    **/
-  void fill_genotypes() {
-#pragma omp parallel for num_threads(n_threads) shared(fmt_lines, variants)
+  void parse_block() {
+#pragma omp parallel for num_threads(n_threads) shared(vcf_records, variants)
     for (uint i = 0; i < to_parse; ++i) {
       fill_variant(i);
     }
